51f0d6e6882c0c7580147c6b460991848ce2155c
hu <yuandong.hu@partner.bmw.de>
2024-11-20 10:00:39 +0100

    publish modules

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,38 @@
-output "module_vpc_id" {
-  value = module.vpc.vpc_id
+# 1.vpc id
+# 2. 
+
+output "output_subnets" {
+  value = local.output_public_subnets
 }
 
-output "module_public_subnet" {
-  value = module.vpc.output_subnets
+output "vpc_id" {
+  description = "The AWS ID from the created VPC"
+  value       = aws_vpc.this.id
+
+}
+
+locals {
+  output_public_subnets = {
+    for key in keys(local.public_subnets) : key => {
+      subnet_id      = aws_subnet.this[key].id
+      available_zone = aws_subnet.this[key].availability_zone
+    }
+  }
+  output_private_subnets = {
+    for key in keys(local.private_subnets) : key => {
+      subnet_id      = aws_subnet.this[key].id
+      available_zone = aws_subnet.this[key].availability_zone
+    }
+  }
+}
+
+output "public_subnets" {
+  description = "The ID and the availability zone of public subnets"
+  value       = local.output_public_subnets
 }
 
 
-output "module_private_subnets" {
-  value = module.vpc.private_subnets
+output "private_subnets" {
+  description = "The ID and the availability zone of private subnets"
+  value       = local.output_private_subnets
 }
--- providers.tf
+++ providers.tf
@@ -1,14 +1,8 @@
 terraform {
-  required_version = ">=1.8, < 2.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
-
-#ami-0556dfb1a147512ba
-provider "aws" {
-  region = "eu-central-1"
 }
